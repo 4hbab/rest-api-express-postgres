8a10409a0d92b5a4e00a3ee7f9481ff96012fd8c
4hbab <sakifahbab2@iut-dhaka.edu>
2023-04-27 21:58:28 +0600

    update: connection link for external database

--- config/db.config.js
+++ config/db.config.js
@@ -3,11 +3,10 @@ dotenv.config()
 
 const Pool = require('pg').Pool
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DBConfigLink,
+    ssl: {
+        rejectUnauthorized: false
+    }
 })
 
 module.exports = pool;
